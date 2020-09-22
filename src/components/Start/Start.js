b65c44b1c5411078d9dce87f904c5c3ed7dd9a24
Oliver Ballon <oliverballon1@gmail.com>
2020-09-22 10:13:50 +0100

    shuffle logic added

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -7,9 +7,11 @@ class Start extends Component {
     this.state = {
       playerName: "",
       players: [],
+      pairs: [],
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleShuffle = this.handleShuffle.bind(this);
   }
 
   handleChange = (e) => {
@@ -25,19 +27,28 @@ class Start extends Component {
     });
   };
 
+  handleShuffle = () => {
+    const { players } = this.state;
+    this.setState({
+      players: players.sort(() => Math.random() - 0.5),
+    });
+  };
+
   render() {
     const { players, playerName } = this.state;
 
     return (
       <div>
-        <form>
+        <form onSubmit="this.reset()">
           <input value={playerName} onChange={this.handleChange} />
           <Button
             label="Add A Player"
             type="submit"
             handleClick={this.handleAdd}
+            disabled={players.length === 8 ? true : false}
           />
         </form>
+        <Button label="Shuffle" handleClick={this.handleShuffle} />
         <PlayerList players={players} />
       </div>
     );
