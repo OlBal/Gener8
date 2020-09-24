abff3ff9c20de92a7825c0506dfe41866baee550
Oliver Ballon <oliverballon1@gmail.com>
2020-09-24 10:44:11 +0100

    layout for games roster grid and flexbox established, title font added

--- src/components/Tournament/Tournament.js
+++ src/components/Tournament/Tournament.js
@@ -21,28 +21,31 @@ class Tournament extends Component {
     return (
       <>
         <div className="container__tournament">
-          <h1> PING PONG</h1>
+          <h1 className="title__App"> PING PONG</h1>
           <h2> TOURNAMENT</h2>
           <section className="container__roster round-one">
-            <article className="container__round-one">
-              <div className="pairs pair__one">
-                <h1>{pairs[0][0]}</h1>
-                <h1>{pairs[0][1]}</h1>
-              </div>
-              <div className="pairs pair__two">
-                <h1>{pairs[1][0]}</h1>
-                <h1>{pairs[1][1]}</h1>
-              </div>
-              <div className="pairs pair__three">
-                <h1>{pairs[2][0]}</h1>
-                <h1>{pairs[2][1]}</h1>
-              </div>
-              <div className="pairs pair__three">
-                <h1>{pairs[3][0]}</h1>
-                <h1>{pairs[3][1]}</h1>
-              </div>
-            </article>
+            <div className="pairs pair__one">
+              <h1 className="title__player">{pairs[0][0]}</h1>
+              <h1 className="title__player">{pairs[0][1]}</h1>
+            </div>
+            <div className="pairs pair__two">
+              <h1 className="title__player">{pairs[1][0]}</h1>
+              <h1 className="title__player">{pairs[1][1]}</h1>
+            </div>
+            <div className="pairs pair__three">
+              <h1 className="title__player">{pairs[2][0]}</h1>
+              <h1 className="title__player">{pairs[2][1]}</h1>
+            </div>
+            <div className="pairs pair__three">
+              <h1 className="title__player">{pairs[3][0]}</h1>
+              <h1 className="title__player">{pairs[3][1]}</h1>
+            </div>
           </section>
+
+          <section className="container__roster semi-final"></section>
+
+          <section className="container__roster final"></section>
+
           <Link to={"/"} className="nav-link nav__tournament">
             <Button label="Settings" handleClick={this.handleBack} />
           </Link>
