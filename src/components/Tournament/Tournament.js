b31f6f2d4efe4664438802f7d7edb9dd7bf9e786
Oliver Ballon <oliverballon1@gmail.com>
2020-09-24 09:35:50 +0100

    Tournament pairs separated and grid established

--- src/components/Tournament/Tournament.js
+++ src/components/Tournament/Tournament.js
@@ -20,17 +20,31 @@ class Tournament extends Component {
     const { pairs } = this.props;
     return (
       <>
-        <div className="container">
+        <div className="container__tournament">
           <h1> PING PONG</h1>
           <h2> TOURNAMENT</h2>
-
-          <h1>{pairs[0]}</h1>
-          <h1>{pairs[1]}</h1>
-          <h1>{pairs[2]}</h1>
-          <h1>{pairs[3]}</h1>
-
+          <section className="container__roster round-one">
+            <article className="container__round-one">
+              <div className="pairs pair__one">
+                <h1>{pairs[0][0]}</h1>
+                <h1>{pairs[0][1]}</h1>
+              </div>
+              <div className="pairs pair__two">
+                <h1>{pairs[1][0]}</h1>
+                <h1>{pairs[1][1]}</h1>
+              </div>
+              <div className="pairs pair__three">
+                <h1>{pairs[2][0]}</h1>
+                <h1>{pairs[2][1]}</h1>
+              </div>
+              <div className="pairs pair__three">
+                <h1>{pairs[3][0]}</h1>
+                <h1>{pairs[3][1]}</h1>
+              </div>
+            </article>
+          </section>
           <Link to={"/"} className="nav-link nav__tournament">
-            <Button label="back" handleClick={this.handleBack} />
+            <Button label="Settings" handleClick={this.handleBack} />
           </Link>
         </div>
       </>
